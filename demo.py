42e1365d4094a4cb3f04b29888ccd2b8cd6af95f
sandhya-codes <gunduboinasandhya2004@gmail.com>
2025-01-21 19:44:28 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,31 @@
+# x = 1
+# y = 2
+# z = 3
+# num1 =3.09
+# print(x+y)
+# y=8
+# z=1
+# print(y+x)
+# y=0
+# print(x+z)
+# print(type(z))
+# print(type(num1))
+# x=5
+# print(5*5)
+# print(5**3)
+# print(y/num1)
+# print(y // num1)
+# print("bad_morning") 
+# string1 ="sandhya"
+# print("string1")
+# print(string1)
+num1 = "123.4"
+word ="sandhya sandhya sandhya"
+print(num1)
+print(len(num1))
+print(len("sandhya1424"))
+print(type(len(" ")))
+print(len(word))
+print(word[22])
+print(word[0:7])#slicing negative index
+print(num1 +"sandhya")
