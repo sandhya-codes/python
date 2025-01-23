d3a7f42e86ea08ea01997d51739b5a979c438276
sandhya-codes <gunduboinasandhya2004@gmail.com>
2025-01-23 17:32:15 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,99 @@
+# print("hello world")
+# num1=123
+# num2=390
+# print(num1+num2)
+# print(num1-num2)
+# num1=000
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+num2=111
+# print(num1*num2)
+word ="print the word"
+print(word)
+print(type(word))
+print(len(word))
+print(word[1])
+# print(word[16])string out of range
+print(word[1:7])
+x="123456"
+print(x)
+print(type(x))
+y="theerrorisstringoutofrange"
+print(len(y))
+print(y[1:14])
+print(y[-10])
+print(y[10:26])
+print(y[-1:-5:-1])
+num1 ="sandhya"
+print(id(num1))
+num2 ="sandhya"
+print(id(num2))
+num3 ="sandhya"
+print(id(num3))
+num1 =2+5j
+num2 =3+5j
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1**num2)
+# print(num1//num2)
+atm_pin = 123
+print(atm_pin == 124)
+print(atm_pin == 123)
+print(3.5>3)
+print(3.5<3)
+print(3.5>=3)
+db_user_password = "1234"
+user_password_input ="12344"
+# if(db_user_password == user_password_input):
+if True:
+ print("login successfully")
+if False:
+ print("wrong password")
+# else
+# print("wrong password")
+#list-----------------------------------------------------------
+list_new = [2,3,5,5,"string",True, [2,3,6,[1,23,5]]]
+print(type(list_new))
+print(list_new)
+print(list_new[0])
+print(list_new[1])
+print(list_new[2])
+print(list_new[3])
+print(list_new[4])
+print(list_new[5])
+print(list_new[6])
+# print(list_new[7])
+print(len(list_new))
+print(list_new[0:4])
+print(list_new[4 : 0 : 1])
+print(list_new[4 : 0 : -1])
+print(len(list_new[6][3]))
+print(list_new[6][3])
+list_new[6][3] = "good morning"
+print(list_new[6][3])
+print(list_new)
+#tuple-----------------------------------------------------------------
+tuple1 = (1,2,3,4,[1.0,7,9,9],"string")
+print(type(tuple1))
+print(len(tuple1))
+print(tuple1[0])
+print(tuple1[1])
+print(tuple1[2])
+print(tuple1[3])
+print(tuple1[4])
+print(tuple1[5])
+print(tuple1[5][2])
+tup_1 = (11,12,30,'string',('sandy', 1, 0)) 
+print(len(tup_1)) 
+print(type(tup_1)) 
+print(tup_1[2]) 
+print(tup_1[-4])
+print(tup_1[-4:-1]) 
+print(tup_1[-1:-2:-1]) 
+#difference b\w tuple and list
+#strings are mutable or not why?
+
