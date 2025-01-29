a58bfa6990de12bfa3d4da7cc20a62220053e3af
sandhya-codes <gunduboinasandhya2004@gmail.com>
2025-01-29 21:47:48 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,36 @@
+# name = input("enter the string")
+# age = int(input("enter the age"))
+# print(age)
+# print(name,"type of",name ,"is :",type(name))
+int1 = 5.6
+int1 = float(int1)
+print(int1)
+int1 = int(int1)
+print(int1)
+int1 = float(int1)
+print(int1)
+#bool
+# bool to numeric
+bool1 =True
+print(int(bool1))
+# numeric to bool
+print(bool(1))
+print(bool(99))
+# bool to string 
+print(bool(" "))
+print(bool("hello"))
+print(bool(''))
+#string to int
+print(int('25'))
+# print(int('25a'))
+print(float('24.33'))
+# print(int('24.33'))
+#string to float
+int1 = float (input())
+print(int1)
+#string to list
+str1 = "sandhya sandhya sandhya"
+print(list(str1))
+#list to string
+list1 =[12,3,4.4,[1,8,94,],True]
+print(str(list1))
